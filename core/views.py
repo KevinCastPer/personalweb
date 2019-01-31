0d2291e97b2305ae2a7317b3db4cb1d1e9265144
KevinCastPer <kecasper@gmail.com>
2019-01-31 17:25:28 +0100

    otro mas practicos, añadir Portafolio y Contacto

--- core/views.py
+++ core/views.py
@@ -5,7 +5,9 @@ html_base = """
 <h1>Mi Web Personal</h1>
 <ul>
     <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portfolio/">Portafolio</a></li>
+    <li><a href="/contact/">Contacto</></a></li>
 </ul>
 """
 
@@ -19,4 +21,16 @@ def about(request):
     return HttpResponse(html_base + """
         <h2>Acerca de</h2>
         <p>Me llamo Kevin y soy programador</p>
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+        <h2>Portafolio</h2>
+        <p>Algunos de mis trabajos</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aqui os dejo mi email para preguntarme dudas: <a href="mailto:hola@gmail.com">hola@gmail.com</a></p>
     """)
