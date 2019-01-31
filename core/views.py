4713a80647ddf380a3a232b62716101d0b775684
KevinCastPer <kecasper@gmail.com>
2019-01-31 17:45:41 +0100

    HttpResponse sistituye por Render y crear 4 paginas webs home, about, portafolio y contacto.

--- core/views.py
+++ core/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-html_base = """
-<h1>Mi Web Personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contacto</></a></li>
-</ul>
-"""
 
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Portada</h2>
-        <p>Esto es la portada</p>
-    """)
+    return render(request, 'core/home.html')
 
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Me llamo Kevin y soy programador</p>
-    """)
+    return render(request, 'core/about.html')
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-        <h2>Portafolio</h2>
-        <p>Algunos de mis trabajos</p>
-    """)
+    return render(request, 'core/portfolio.html')
 
 def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aqui os dejo mi email para preguntarme dudas: <a href="mailto:hola@gmail.com">hola@gmail.com</a></p>
-    """)
+    return render(request, 'core/contact.html')
